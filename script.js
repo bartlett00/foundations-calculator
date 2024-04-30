9228f2e6feea09e59be18302d5815fe10ee1c92e
Pax <113464089+bartlett00@users.noreply.github.com>
2024-04-30 15:32:27 -0500

    Add event listeners for operator buttons,     numbers, and the equals button

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let operandOne;
-let operator;
-let operandTwo;
-let display;
+let operandOne = '';
+let operator = '';
+let operandTwo = '';
+let display = '0';
+let result = '';
 
 function add(a, b) {
  return a + b;
@@ -33,16 +34,48 @@ function operate(operator) {
         case '/':
             
             break;
-        case '=':
-
-            break;
-        case 'AC':
-
-            break;
         default:
             
     }
 }
 
 const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operator')
+const operators = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+const clear = document.querySelector('#AC');
+
+console.log(numbers);
+console.log(operators);
+
+numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (display === '0') {
+            display = button.id;
+            operandOne = button.id;
+        }
+        if (operator == '') {
+           operandOne = operandOne.concat(button.id);
+        } else if (operator != '') {
+            operandTwo = operandTwo.concat(button.id);
+        }
+    display = display.concat(button.id);
+    console.log(operandOne);
+    console.log(operandTwo);
+    }
+    );
+});
+
+operators.forEach((button) => {
+    button.addEventListener('click', () => {
+        operator = button.id;
+        display = display.concat(` ${operator} `);
+    });
+});
+
+equals.addEventListener('click', () => {
+    result = operate(operator);
+    display = result;
+    operandOne = result;
+    operandTwo = '';
+});
+
